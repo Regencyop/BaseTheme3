3ddabe852beda44e20919d9c8e70cacf515f1c5e
Regencyop <GitHub@regencyop.com>
2015-10-22 11:11:46 -0400

    Update tree.js

--- app/js/directives/tree.js
+++ app/js/directives/tree.js
@@ -1,172 +1,32 @@
-    /* --------------Main Site Colors-------------- */
+four51.app.directive('categorytree', function() {
+	var obj = {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			tree: '=',
+            current: '='
+		},
+		templateUrl: 'partials/categoryTree.html'
+	};
+	return obj;
+});
 
-	body {
-		background: url('http://www.four51.com/Themes/Custom/df7e2b71-6326-4da6-a24f-554d7d910faf/backgroundImage/background-color-mezzoPix-grayscale.jpg') no-repeat center center fixed;
-	    -webkit-background-size: cover;
-		-moz-background-size: cover;
-		-o-background-size: cover;
-		background-size: cover;
-		/*
-		color: #bbb;
-        background-color: white !important; */
-    }
-    
-    .companyImageBackground {
-        background-color: white !important;
-    }
-    
-    .container-view {
-        background-color: rgba(255, 255, 255, 1);
-        
-    }
-    
-    /* --------------Category Icon Colors-------------- */
-    
-    .panel-default {
-        background-color: white !important;
-    }
-    
-    .panel-category-list {
-        background-color: white !important;
-    }
-    
-    /*to hide category Names, display: none; */
-    
-    .panel-category-list ul li .well h5 {
-        /* display: none !important; */
-    }
-    
-    .navColors {
-        color: black !important;
-    }
-    
-    /* --------------navbar Colors-------------- */
-    
-    /*Navbar Background*/
-    
-    .navColorControl {
-        background-color: black !important;
-    }
-    
-    /* Links on left side */
-    .colorControl {
-        color: white !important;
-        background-color: black !important;
-    }
-    
-    .colorControl:hover {
-        color: white !important;
-        background-color: grey !important;
-    }
-    
-    /* Category Dropdown Menu */
-    .dropdown-menu {
-        color: white !important;
-        background-color: grey !important;
-    }
-    
-    .dropdownColor {
-        color: white !important;
-        background-color: grey !important;
-    }
-    
-    .dropdownColor:hover {
-        color: white !important;
-        background-color: grey !important;
-        font-size: 105% !important;
-        text-decoration: none !important;
-    }
-    
-    /* backArrow on Left */
-    
-    .backArrow {
-        color: white !important;
-    }
-    
-    .backArrow:hover {
-        color: darkgrey !important;
-    }
-    
-    /* Shopping Cart Badges on Right */
-    
-    .cartPulledRightBackground {
-        background-color: black !important;
-    }
-    
-    .cartCountControl{
-        background-color: grey !important;
-        color: white !important;
-    }
-    
-    .cartCountControl:hover {
-        background-color: darkgrey !important;
-        color: white !important;
-    }
-    
-    .shoppingCartBadge {
-        background-color: grey !important;
-        border: none !important;
-    }
-    
-    .shoppingCartBadge:hover {
-        background-color: darkgrey !important;
-        color: white !important;
-    }
-    
-    /* --------------Checkout Colors-------------- */
-    
-    /*buttons*/
-    
-    .cancelColor {
-        background-color: grey !important;
-    }
-    
-    .cancelColor:hover {
-        background-color: #505050 !important;
-    }
-    
-    .saveColor {
-        background-color: grey !important;   
-    }
-    
-    .saveColor:hover { 
-        background-color: #505050 !important;
-    }
-    
-    .submitColor {
-        background-color: #505050 !important;
-    }
-    
-    .submitColor:hover {
-        background-color: #222 !important;
-    }
-    
-    .deleteItem {
-        background-color: red !important;
-        border: none !important;
-    }
-    
-    .deleteItem:hover {
-        background-color: darkred !important;
-        border: none !important;
-    }
-    
-    
-    /* --------------Quick Edits-------------- */
-    
-    /* Dropdown Width */
-    
-    .navbar .container .navbar-nav.pull-left li ul li,
-    .navbar .container-view .navbar-nav.pull-left li ul li {
-        width: 200px;
-    }
-    
-    /* Company Image */
-    
-    .jumbotron h2 img {
-	    max-height: 100%;
-    	background-color: white;
-    }
-    
-    
-    
+four51.app.directive('node', ['$compile', function($compile) {
+	var obj = {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			node: '=',
+            current: '='
+		},
+		template: '<li class="451_cat_item dropdownColor" ng-class="{\'active\':  current.InteropID == node.InteropID}"><a ng-href="catalog/{{node.InteropID}}" ng-bind-html="node.Name"class="dropdownColor"></a></li>',
+		link: function(scope, element) {
+			if (angular.isArray(scope.node.SubCategories)) {
+			    //replace Categories with SubCategories for full list
+				element.append("<categorytree class='colorControl' tree='node.Categories' current='current'/>");
+				$compile(element.contents())(scope);
+			}
+		}
+	};
+	return obj;
+}]);

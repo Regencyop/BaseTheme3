b2b0513a781e77f0fbe2adcbea518d1cfe6271a9
Regencyop <GitHub@regencyop.com>
2016-01-21 16:45:22 -0500

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -2,3 +2,4 @@
 
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-BackToTop', 'TabsView', 'OrderCloud-SpecForms', 'OrderCloud-AddToCartPreview', 'OrderCloud-ProductMatrix']);
+//only update app.js if absolutely necessary
